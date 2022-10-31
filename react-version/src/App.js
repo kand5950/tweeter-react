53b5773af6fcdab00d18fb6c31e3105b640b9b13
Anit <anittankandasamy@gmail.com>
2022-10-30 20:53:40 -0400

    added import css to app.js

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -2,6 +2,7 @@ import Navigation from "./components/Navigation";
 import Profile from "./components/Profile";
 import TweetList from "./components/TweetList";
 import TweetForm from "./components/TweetForm";
+import './App.css';
 
 function App() {
   return (
