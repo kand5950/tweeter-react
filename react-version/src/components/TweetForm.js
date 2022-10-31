28e13d6e4e9a740151f9a34cec0b26f82b0efed4
Anit <anittankandasamy@gmail.com>
2022-10-30 22:31:09 -0400

    disabling submit behavior on the Form

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,12 +1,15 @@
 function TweetForm(){
     const placeholder = "What are you humming about?"
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
     return (
-        <section class="newtweet">
-      <form method="post" action="/tweets" class="newtweet__form">
-        <textarea class="form__textarea" name="text" placeholder={placeholder}></textarea>
-        <input type="submit" value="Tweet" class="form__input"></input>
-        <span class="form__counter">140</span>
+        <section className="newtweet">
+      <form onSubmit={handleSubmit} method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder={placeholder}></textarea>
+        <input type="submit" value="Tweet" className="form__input"></input>
+        <span className="form__counter">140</span>
       </form>
     </section>
     );
